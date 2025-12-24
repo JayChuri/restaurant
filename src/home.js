141b549c73b13bd6a3c4a019517890eabf1361ca
JayChuri <jaychuri6494@gmail.com>
2025-12-24 22:03:58 +0530

    Home page Done

---
+++ src/home.js
@@ -0,0 +1,31 @@
+import "./home.css";
+import imageJuice from "./juice.gif";
+
+export function createHome() {
+    const mainContentDiv = document.createElement("div");
+    mainContentDiv.id = "main_content";
+
+    const imageDiv = document.createElement("img");
+    imageDiv.src = imageJuice;
+    imageDiv.alt = "";
+    mainContentDiv.appendChild(imageDiv);
+
+    const contentDiv = document.createElement("div");
+    contentDiv.className = "content";
+
+    const h1Div = document.createElement("h1");
+    h1Div.textContent = "Welcome to Love Mix Cafe";
+    contentDiv.appendChild(h1Div);
+
+    const hrDiv = document.createElement("hr");
+    contentDiv.appendChild(hrDiv);
+
+    const pDiv = document.createElement("p");
+    pDiv.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa hic sint corrupti deserunt. Explicabo reiciendis error natus! Sapiente, vitae quos? Aspernatur rerum excepturi magnam aperiam omnis maxime vero laborum dicta!";
+    contentDiv.appendChild(pDiv);
+
+    mainContentDiv.appendChild(contentDiv);
+
+    return mainContentDiv;
+}
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./index.css"
+import { createHome } from "./home";
 console.log("Hello World!");
+
+const home_button = document.getElementById("home_button");
+home_button.addEventListener("click", function () {
+    remove_main_content();
+    document.body.appendChild(createHome());
+    
+})
+
+function remove_main_content() {
+    const main_content = document.getElementById("main_content");
+    if (main_content) {
+        main_content.remove();
+    }
+}
+
+document.body.appendChild(createHome());
